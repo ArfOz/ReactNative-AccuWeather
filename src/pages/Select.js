770cb50e2c2c2366a2be3790ac4a0b1f2c2fbec7
ArfOz <arfoz1245@gmail.com>
2021-01-14 01:38:54 +0300

    switc case added

--- src/pages/Select.js
+++ src/pages/Select.js
@@ -1,6 +1,6 @@
 
 import React ,{useState, useEffect} from "react";
-import {Text, SafeAreaView, View,TouchableOpacity} from "react-native";
+import {Text, SafeAreaView, View,TouchableOpacity, Image} from "react-native";
 
 const Select = (props) => {
     const {secilen} = props.route.params;
@@ -17,14 +17,84 @@ const Select = (props) => {
                 <Text>City:{secilen.LocalizedName}</Text>
                 <Text>Temperature: {secilen.Temperature.Metric.Value}</Text>
                 <Text>Weather Conditions: {secilen.WeatherText}</Text>
-                {/* switch ({secilen.WeatherText}) {
-                    case "Mostly cloudy":
-                        
-                        break;
-                
-                    default:
-                        break;
-                } */}
+                <View>
+
+                {(() => {
+  
+                    switch (secilen.WeatherText) {
+                        case "Mostly cloudy":
+                             return (
+                                <Image source={require('../assets/mostlyCloudy.png')} />
+                            )
+                        case "Partly cloudy":
+                            return (
+                                <Image source={require('../assets/cloudy.png')} />
+                            )
+                        case "Hazy sunshine":
+                            return (
+                                <Image source={require('../assets/hazySunshine.png')} />
+                            )
+                        case "Clouds and sun":
+                            return (
+                                <Image source={require('../assets/sunandClouds.png')} />
+                            )
+                        case "Cloudy":
+                            return (
+                                <Image source={require('../assets/cloudy.png')} />
+                            )
+                        case "Snow":
+                            return (
+                                <Image source={require('../assets/snow.png')} />
+                            )
+                        case "Mostly clear":
+                            return (
+                                <Image source={require('../assets/sunny.png')} />
+                            )
+                        case "Drizzle":
+                            return (
+                                <Image source={require('../assets/drizzle.png')} />
+                            )
+                        case "Foggy":
+                            return (
+                                <Image source={require('../assets/foggy.png')} />
+                            )
+                        case "Some clouds":
+                            return (
+                                <Image source={require('../assets/cloudy.png')} />
+                            )
+                        case "Mostly Sunny":
+                            return (
+                                <Image source={require('../assets/sunny.png')} />
+                            )
+                        case "Clear":
+                            return (
+                                <Image source={require('../assets/clear.png')} />
+                            )
+                        case "Overcast":
+                            return (
+                                <Image source={require('../assets/overcast.png')} />
+                            )
+                        case "A shower":
+                            return (
+                               <Image source={require('../assets/aShower.png')} />
+                            )
+                        case "Light snow":
+                            return (
+                                <Image source={require('../assets/snow.png')} />
+                            )
+                        case "Rain":
+                            return (
+                                <Image source={require('../assets/rain.png')} />
+                            )
+                        default:
+                            return (                      
+                                <Image source={require('../assets/noImage.png')} />
+                             )
+                    }
+
+                })()}
+                 
+                </View>
 
 
             </View>
